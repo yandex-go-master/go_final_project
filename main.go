be652f668d980fe78ad23122f0cdc103277af6c0
yandex-go-master <abdullinov.a@yandex.kz>
2024-12-22 19:16:33 +0500

    add .env

--- main.go
+++ main.go
@@ -2,14 +2,27 @@ package main
 
 import (
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+
+	port := os.Getenv("TODO_PORT")
+	if len(port) == 0 {
+		port = "7540"
+	}
+
 	webDir := "./web"
 
 	http.Handle("/", http.FileServer(http.Dir(webDir)))
 
-	err := http.ListenAndServe(":7540", nil)
+	err = http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		panic(err)
 	}
